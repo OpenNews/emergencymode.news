b8a1d5bda8b96dbd58d1252be9fb9f67a8683d66
Tiff Fehr <60173+tiffehr@users.noreply.github.com>
2026-06-03 23:49:53 +0000

    cleanup and typos on placeholder site-wide client JS file in site-styles

--- plugins/emfn-site-styles-plugin/assets/js/emfn-site-styles-plugin.js
+++ plugins/emfn-site-styles-plugin/assets/js/emfn-site-styles-plugin.js
@@ -1,5 +1,5 @@
 // @ts-check (drives Type checking in modern editors)
-// EMFN Site Styles Plugin - client-side behaviors
+// EMFN Site Styles Plugin - site-wide client-side behaviors
 "use strict";
 
 /**
@@ -7,11 +7,11 @@
  * @typedef {import("../../../shared/emfn-types").EmfnWindow} EmfnWindow
  */
 
-const version = "0.0.01"; // debugging versioning
+const version = "0.0.01"; // debugging version notes for JS only
 const emfnWindow = /** @type {EmfnWindow} */ (window);
 
 /* ********* INITIALIZATION **********
- * - TK things ot do
+ * - TK site-wide client-side tasks, if any
  */
 
 const isEMFNHost = window.location.host.includes("emergencymode");
